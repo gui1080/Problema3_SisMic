04471f4c41e506433ba75f0d1741a8adc65a732d
Guilherme Braga <gbp1999@gmail.com>
2019-11-11 19:25:52 -0300

    comentários

--- Problema_3/source/converte_string.c
+++ Problema_3/source/converte_string.c
@@ -6,7 +6,7 @@ void reverse(char* str, int len)
     int i = 0, j = len - 1, temp;
     while (i < j) {
         temp = str[i];
-        str[i] = str[j];
+        str[i] = str[j];            // colocamos o valor na ordem correta
         str[j] = temp;
         i++;
         j--;
@@ -17,7 +17,7 @@ int integer_pra_string(int x, char str[], int d)
 {
     int i = 0;
     while (x) {
-        str[i++] = (x % 10) + '0';
+        str[i++] = (x % 10) + '0';              // pegamos o restante em modulos de 10
         x = x / 10;
     }
 
@@ -31,13 +31,13 @@ int integer_pra_string(int x, char str[], int d)
 
 void float_para_string(float n, char* res, int afterpoint)
 {
-    int ipart = (int)n;
+    int ipart = (int)n;   // pegamos a parte inteira
 
-    float fpart = n - (float)ipart;
+    float fpart = n - (float)ipart;        // parte de ponto flutuantes
 
-    int i = integer_pra_string(ipart, res, 1);
+    int i = integer_pra_string(ipart, res, 1);      // valor em inteiro
 
-    if (afterpoint != 0) {
+    if (afterpoint != 0) {                          // casas decimais apos a virgula
         res[i] = ',';
 
         fpart = fpart * pow(10, afterpoint);
