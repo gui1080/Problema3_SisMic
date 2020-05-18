a0ee5b540d1e24ea5078796ddfc2c34cb820ddac
Guilherme B <38567020+gui1080@users.noreply.github.com>
2020-05-18 20:12:06 -0300

    Update main.c

--- Problema_3/Problema_3_final/main.c
+++ Problema_3/Problema_3_final/main.c
@@ -2,7 +2,7 @@
  * Professor: Daniel Café
  * Turma: D
  * Alunos: Guilherme Braga (17/0162290) e Gabriel Matheus (17/0103498)
- * Trabalho no Github: https://github.com/therealguib545/Problema3_SisMic
+ * Trabalho no Github: https://github.com/gui1080/Problema3_SisMic
  *
  */
 
